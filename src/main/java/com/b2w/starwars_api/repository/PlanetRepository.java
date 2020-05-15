812abbcc589d542cf7098ffe00638bdb7ce4a64e
igor rebeche <igorrebeche@gmail.com>
2020-05-15 04:34:43 -0300

    add findByName Filter to Planet Repository

--- src/main/java/com/b2w/starwars_api/repository/PlanetRepository.java
+++ src/main/java/com/b2w/starwars_api/repository/PlanetRepository.java
@@ -1,8 +1,10 @@
 package com.b2w.starwars_api.repository;
 
+import java.util.Optional;
+
 import com.b2w.starwars_api.models.Planet;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface PlanetRepository extends MongoRepository<Planet, String> {
-
+    public Optional<Planet> findByName(String name);
 }
